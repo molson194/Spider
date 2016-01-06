d3da0fe277f512ab2633a2748bc838ae9b90b1d8
Matthew Olson <Olson@matthews-macbook-air-2.local>
2016-01-05 21:06:26 -0700

    Initial Commit

---
+++ Spider/AppDelegate.swift
@@ -0,0 +1,38 @@
+//
+//  AppDelegate.swift
+//  Spider
+//
+//  Created by Matthew Olson on 1/5/16.
+//  Copyright (c) 2016 Molson. All rights reserved.
+//
+
+
+import Cocoa
+import SpriteKit
+
+@NSApplicationMain
+class AppDelegate: NSObject, NSApplicationDelegate {
+    
+    @IBOutlet weak var window: NSWindow!
+    @IBOutlet weak var skView: SKView!
+    
+    func applicationDidFinishLaunching(aNotification: NSNotification) {
+        /* Pick a size for the scene */
+        if let scene = GameScene(fileNamed:"GameScene") {
+            /* Set the scale mode to scale to fit the window */
+            scene.scaleMode = .AspectFill
+            
+            self.skView!.presentScene(scene)
+            
+            /* Sprite Kit applies additional optimizations to improve rendering performance */
+            self.skView!.ignoresSiblingOrder = true
+            
+            self.skView!.showsFPS = true
+            self.skView!.showsNodeCount = true
+        }
+    }
+    
+    func applicationShouldTerminateAfterLastWindowClosed(sender: NSApplication) -> Bool {
+        return true
+    }
+}
---
+++ Spider/GameScene.swift
@@ -0,0 +1,40 @@
+//
+//  GameScene.swift
+//  Spider
+//
+//  Created by Matthew Olson on 1/5/16.
+//  Copyright (c) 2016 Molson. All rights reserved.
+//
+
+import SpriteKit
+
+class GameScene: SKScene {
+    override func didMoveToView(view: SKView) {
+        /* Setup your scene here */
+        let myLabel = SKLabelNode(fontNamed:"Chalkduster")
+        myLabel.text = "Hello, World!"
+        myLabel.fontSize = 45
+        myLabel.position = CGPoint(x:CGRectGetMidX(self.frame), y:CGRectGetMidY(self.frame))
+        
+        self.addChild(myLabel)
+    }
+    
+    override func mouseDown(theEvent: NSEvent) {
+        /* Called when a mouse click occurs */
+        
+        let location = theEvent.locationInNode(self)
+        
+        let sprite = SKSpriteNode(imageNamed:"Spaceship")
+        sprite.position = location;
+        sprite.setScale(0.5)
+        
+        let action = SKAction.rotateByAngle(CGFloat(M_PI), duration:1)
+        sprite.runAction(SKAction.repeatActionForever(action))
+        
+        self.addChild(sprite)
+    }
+    
+    override func update(currentTime: CFTimeInterval) {
+        /* Called before each frame is rendered */
+    }
+}
