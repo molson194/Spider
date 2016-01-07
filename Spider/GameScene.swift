2b8b5327a4516024afc015fdb71c30b00ec6e25e
Matthew Olson <Olson@Matthews-MacBook-Air-2.local>
2016-01-07 00:39:30 -0700

    Adding new row of cards

--- Spider/GameScene.swift
+++ Spider/GameScene.swift
@@ -9,12 +9,14 @@
 import SpriteKit
 import Foundation
 import GameplayKit
+
 var fieldCards:[[Card]] = [[Card]]()
 var deck:[Card] = [Card]()
 
 class GameScene: SKScene {
     var cardValue: [Int] = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13]
     var cardSuit: [String] = ["Diamonds","Spades", "Hearts", "Clubs","Diamonds","Spades", "Hearts", "Clubs"]
+    var newCards = SKSpriteNode.init()
     
     override func didMoveToView(view: SKView) {
         let myLabel = SKLabelNode(fontNamed:"Chalkduster")
@@ -22,6 +24,11 @@ class GameScene: SKScene {
         myLabel.fontSize = 45
         myLabel.position = CGPoint(x:CGRectGetMidX(self.frame), y:CGRectGetMidY(self.frame))
         self.addChild(myLabel)
+        
+        let texture = SKTexture(imageNamed: "CardBack")
+        newCards = SKSpriteNode(texture: texture, color: NSColor.clearColor(), size: CGSize(width: 100,height: 150))
+        newCards.position = CGPoint(x: 1200, y: 150)
+        self.addChild(newCards)
 
         //TODO: if 1 deck-> cardSuit has spades...Keep adding cards until 8 sets
         let cardSet = NSImage(named: "CardSet")
@@ -61,6 +68,23 @@ class GameScene: SKScene {
             card!.position = CGPointMake(CGFloat(125*x+80),CGFloat(700 - 35*y))
             self.addChild(card!)
             i++
+        }
+    }
+    
+    override func mouseUp(theEvent: NSEvent) {
+        var i = 0
+        while i < 10 {
+            let card = deck.first
+            let numCards = fieldCards[i].count
+            card?.zPosition = CGFloat(numCards)
+            fieldCards[i].append(card!)
+            deck.removeFirst()
+            card!.position = CGPointMake(CGFloat(125*i+80),CGFloat(700 - 35*numCards))
+            self.addChild(card!)
+            i++
+        }
+        if deck.count==0 {
+            self.removeChildrenInArray([newCards])
         }
     }
     
